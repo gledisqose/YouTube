9a3bfead6d716c9bbdc44cc263a17c84fc3a6ccf
gledisqose <51320175+gledisqose@users.noreply.github.com>
2019-07-31 00:54:31 +0200

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,31 @@
+<html>
+<body>
+
+<?php
+
+$dbname = 'example';
+$dbuser = 'root';  
+$dbpass = ''; 
+$dbhost = 'localhost'; 
+
+$connect = @mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+
+if(!$connect){
+	echo "Error: " . mysqli_connect_error();
+	exit();
+}
+
+echo "Connection Success!<br><br>";
+
+$temperature = $_GET["temperature"];
+$humidity = $_GET["humidity"]; 
+
+
+$query = "INSERT INTO iot_project (temperature, humidity) VALUES ('$temperature', '$humidity')";
+$result = mysqli_query($connect,$query);
+
+echo "Insertion Success!<br>";
+
+?>
+</body>
+</html>
